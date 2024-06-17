f2c5a9b4ef755d9eb452e5baff66e9d3e0d243d6
Anselmo2001 <claudio.katzer@catolicasc.edu.br>
2024-06-17 18:17:38 -0300

    página contato
    foi realizada algumas alterações.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/close_white_36dp.svg";
-    }
+const button = document.getElementById('formulario');
+
+button.addEventListener('click', showAlert);
+
+function showAlert() {
+  alert("Confirme o envio da mensagem !");
 }
+
