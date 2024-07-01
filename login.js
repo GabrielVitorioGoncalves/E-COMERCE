6738516c48d2ac1730d332942b5254699425426b
AndressaLp <andressarodrigueslopes157@gmail.com>
2024-07-01 11:21:36 -0300

    finalizado

--- login.js
+++ login.js
@@ -1,11 +1,21 @@
-document.getElementById("openModal").addEventListener("click", function() {
-    document.getElementById("modal").style.display = "flex";
-});
-
-document.querySelector(".confirm").addEventListener("click", function() {
-    document.getElementById("modal").style.display = "none";
-});
-
 function novaSenha(){
     window.location.href = "novaSenha.html"
 }
+
+function verificar() {
+    const inputText = document.getElementById('email').value.trim();
+    const modal = document.getElementById('modal');
+    const emailInput = document.getElementById('email');
+
+    if (inputText !== '') {
+        modal.style.display = 'flex';
+        emailInput.setCustomValidity('');
+        return true
+    } else {
+        document.getElementById('email').setCustomValidity('Preencha este campo');
+                return false; 
+    }
+}
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
